e3c7edb9a45bfb3aefb0b8b207b92d6eb32a2869
Lillybliss <lillyallgood@ucf.edu>
2024-03-11 15:01:35 -0400

    Set up main pages

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./Styles/App.css";
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
+import Home from "./Home";
+import Contact from "./Contact.js";
+import Resume from "./Resume.js";
+import Portfolio from "./Portfolio.js";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Testing 1</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+        </Routes>
+
+        <Footer />
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
