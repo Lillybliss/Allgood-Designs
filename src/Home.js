1ea68d56db886a931fd5c9cb1548893048d05b90
Lillybliss <lillyallgood@ucf.edu>
2024-03-12 18:13:21 -0400

    starting home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { Button } from "react-bootstrap";
+import logo from "./Assets/lightblue.svg";
+import headshot from "./Assets/headshot_circle.png";
+import "./Home.css";
 
 const Home = () => {
   return (
     <div>
       <div className="home-image">
+        <img src={logo} alt="Lilly Allgood Logo" className="homeLogo" />
         <h2>
           Elevating Digital Experiences Through Design, Development, and More...
         </h2>
@@ -13,21 +17,28 @@ const Home = () => {
       </div>
 
       <div className="about">
-        <h1>About Me</h1>
-        <h2>Hello and welcome to my online portfolio!</h2>
-        <p>
-          My name is Lilly Allgood, and I'm a visual designer and web developer
-          based in Orlando, Florida. I'm deeply passionate about crafting
-          exceptional user experiences and leveraging my creativity to enhance
-          the digital world. Currently pursuing a BA in Digital Media at UCF, I
-          have immersed myself in various coding languages and design software.
-          Throughout my academic journey, I've earned a place on the Honor's
-          List, Dean's List, and President's Honor Roll, maintaining a stellar
-          4.0 GPA. Outside of academia, I enjoy painting and showcasing my art
-          in local galleries and markets. I believe in the power of art as a
-          form of self-expression, and I'm committed to using my diverse skills
-          to make a meaningful impact in today's digital landscape.
-        </p>
+        <div className="white-box">
+          <div className="about-text">
+            <h1>About Me</h1>
+            <h2>Hello and welcome to my online portfolio!</h2>
+            <p>
+              My name is Lilly Allgood, and I'm a visual designer and web
+              developer based in Orlando, Florida. I'm deeply passionate about
+              crafting exceptional user experiences and leveraging my creativity
+              to enhance the digital world. Currently pursuing a BA in Digital
+              Media at UCF, I have immersed myself in various coding languages
+              and design software. Throughout my academic journey, I've earned a
+              place on the Honor's List, Dean's List, and President's Honor
+              Roll, maintaining a stellar 4.0 GPA. Outside of academia, I enjoy
+              painting and showcasing my art in local galleries and markets. I
+              believe in the power of art as a form of self-expression, and I'm
+              committed to using my diverse skills to make a meaningful impact
+              in today's digital landscape.
+            </p>
+          </div>
+
+          <img src={headshot} alt="Headshot" className="headshot" />
+        </div>
       </div>
 
       <div className="services">
