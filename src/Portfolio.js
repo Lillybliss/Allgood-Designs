339305e7c229a1df0075cbb837ee4aa4ef23689f
Lillybliss <lillyallgood@ucf.edu>
2024-03-14 18:14:55 -0400

    Blue gif portfolio header

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -7,7 +7,12 @@ const Portfolio = () => {
       <div className="header-container">
         <h1 className="header-title">Portfolio</h1>
       </div>
-      <p>Testing 1</p>
+      <div className="portfolio-container">
+        <p>Testing 1</p>
+        <p>Testing 1</p>
+
+        <p>Testing 1</p>
+      </div>
     </div>
   );
 };
