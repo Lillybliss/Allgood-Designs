aa9aa6cf3526dc4bd98c234154cbce835c42d3a3
Lillybliss <lillyallgood@ucf.edu>
2024-03-20 17:17:21 -0400

    Resume mobile styling done

--- src/Resume.js
+++ src/Resume.js
@@ -114,83 +114,115 @@ const Resume = () => {
 
       <div className="Experience">
         <h2 className="underline">Experience</h2>
-        <p>
-          <strong>BLACK DOG DESIGNS</strong>
-        </p>
-        <p>Design Intern</p>
-        <p>January 2024 - April 2024</p>
-        <ul>
-          <li>
-            Designed engaging layouts for effective email marketing campaigns,
-            enhancing customer outreach.
-          </li>
-          <li>
-            Utilized marketing analytics tools to gather insights and improve
-            campaign performance.
-          </li>
-          <li>
-            Produced digital assets for social media platforms, maintaining
-            brand coherence across various channels.
-          </li>
-          <li>
-            Worked closely with cross-functional teams to understand marketing
-            goals and translated them into visually appealing design solutions.
-          </li>
-          <li>
-            Developed aesthetically pleasing and user-friendly web page designs,
-            focusing on optimal user experience and brand consistency.
-          </li>
-        </ul>
+        {/* 1st row */}
+        <div className="resumeRow">
+          <div className="left-column">
+            <img src={briefcase} alt="Briefcase" className="briefcase-icon" />
+            <div className="vertical-line"></div>
+          </div>
+          <div className="right-column">
+            <p>
+              <strong>BLACK DOG DESIGNS</strong>
+            </p>
+            <p>Design Intern</p>
+            <p>January 2024 - April 2024</p>
+            <ul>
+              <li>
+                Designed engaging layouts for effective email marketing
+                campaigns, enhancing customer outreach.
+              </li>
+              <li>
+                Utilized marketing analytics tools to gather insights and
+                improve campaign performance.
+              </li>
+              <li>
+                Produced digital assets for social media platforms, maintaining
+                brand coherence across various channels.
+              </li>
+              <li>
+                Worked closely with cross-functional teams to understand
+                marketing goals and translated them into visually appealing
+                design solutions.
+              </li>
+              <li>
+                Developed aesthetically pleasing and user-friendly web page
+                designs, focusing on optimal user experience and brand
+                consistency.
+              </li>
+            </ul>
+          </div>
+        </div>
 
-        <p>
-          <strong>Revolution Lash Studio</strong>
-        </p>
-        <p>Manager / Licensed Esthetician</p>
-        <p>July 2019 - December 2023</p>
-        <ul>
-          <li>Led redesign of company's brand identity during rebrand.</li>
-          <li>
-            Created a variety of marketing collateral, including brochures,
-            manuals, and promotional materials, ensuring a cohesive brand image.
-          </li>
-          <li>
-            Assisted in developing monthly promotional campaigns to attract new
-            clients.
-          </li>
-          <li>
-            Designed digital assets and composed copy for marketing campaigns.
-          </li>
-          <li>Fostered friendly, warm, and exceptional customer experience.</li>
-          <li>
-            Analyzed business performance to identify areas of improvement and
-            make necessary adjustments.
-          </li>
-          <li>
-            Trained new estheticians on proper techniques and safety protocols
-            to develop capabilities of individuals and improve team efficiency.
-          </li>
-        </ul>
+        {/* 2nd row */}
+        <div className="resumeRow">
+          <div className="left-column">
+            <img src={briefcase} alt="Briefcase" className="briefcase-icon" />
+            <div className="vertical-line"></div>
+          </div>
+          <div className="right-column">
+            <p>
+              <strong>Revolution Lash Studio</strong>
+            </p>
+            <p>Manager / Licensed Esthetician</p>
+            <p>July 2019 - December 2023</p>
+            <ul>
+              <li>Led redesign of company's brand identity during rebrand.</li>
+              <li>
+                Created a variety of marketing collateral, ensuring a cohesive
+                brand image.
+              </li>
+              <li>
+                Assisted in developing monthly promotional campaigns to attract
+                new clients.
+              </li>
+              <li>
+                Designed digital assets and composed copy for marketing
+                campaigns.
+              </li>
+              <li>
+                Fostered friendly, warm, and exceptional customer experience.
+              </li>
+              <li>
+                Analyzed business performance to identify areas of improvement
+                and make necessary adjustments.
+              </li>
+              <li>
+                Trained new estheticians on proper techniques and safety
+                protocols to develop capabilities of individuals and improve
+                team efficiency.
+              </li>
+            </ul>
+          </div>
+        </div>
 
-        <p>
-          <strong>Dr. Jeanette G Warner, MD</strong>
-        </p>
-        <p>Administrative Assistant</p>
-        <p>February 2018 - July 2019</p>
-        <ul>
-          <li>
-            Conducted insurance verification and pre-authorizations and managed
-            patient charts.
-          </li>
-          <li>
-            Managed over 50 customer calls per day, routed messages and resolved
-            patient inquiries within target timeframes.
-          </li>
-          <li>
-            Built and maintained excellent customer relationships through timely
-            response to inquiries and going above and beyond to accommodate
-            unusual requests.
-          </li>
-        </ul>
+        {/* 3rd row */}
+        <div className="resumeRow">
+          <div className="left-column">
+            <img src={briefcase} alt="Briefcase" className="briefcase-icon" />
+          </div>
+          <div className="right-column">
+            <p>
+              <strong>Dr. Jeanette G Warner, MD</strong>
+            </p>
+            <p>Administrative Assistant</p>
+            <p>February 2018 - July 2019</p>
+            <ul>
+              <li>
+                Conducted insurance verification and pre-authorizations and
+                managed patient charts.
+              </li>
+              <li>
+                Managed over 50 customer calls per day, routed messages and
+                resolved patient inquiries within target timeframes.
+              </li>
+              <li>
+                Built and maintained excellent customer relationships through
+                timely response to inquiries and going above and beyond to
+                accommodate unusual requests.
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
 
       <div className="Education">
@@ -201,7 +233,7 @@ const Resume = () => {
             Central Florida
           </p>
           <p>Expected: May 2024</p>
-          <p>
+          <p className="honors">
             <strong>Honors and Achievements: </strong>Dean's List, Director's
             Honor Roll, and President's Honor Roll (4.0 GPA)
           </p>
