9797c833f9aae878a430497625945cca64c76be9
Lillybliss <lillyallgood@ucf.edu>
2024-03-21 15:15:13 -0400

    Starting contact styles

--- src/Contact.js
+++ src/Contact.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import PopUpContact from "./Components/PopUpContact";
+import "./Styles/Contact.css";
+import email from "./Assets/icons/icons8-email-100.png";
 
 const Contact = () => {
   const [isPopupOpen, setIsPopupOpen] = useState(false);
@@ -13,12 +15,28 @@ const Contact = () => {
 
   return (
     <div className="home-background">
-      <h1>Contact</h1>
-      <p>Testing 1</p>
-      <button style={{ cursor: "pointer" }} onClick={handleImageClick}>
-        Submit
-      </button>
-      <PopUpContact isOpen={isPopupOpen} closePopup={closePopup} />
+      <div className="contact-background">
+        <div className="contact-header-container">
+          <h1 className="header-title">Contact Me</h1>
+          <h2 className="contact-h2">
+            If you're interested in learning more about my work or would like to
+            discuss potential opportunities, please don't hesitate to contact
+            me.
+          </h2>
+          <div className="contact-email">
+            <img src={email} alt="Email" className="contact-email-icon" />
+            <p className="resume-p">Lillyallgood@ucf.edu</p>
+          </div>
+        </div>
+
+        <div className="contact-form-container">
+          <h2>Let's get in touch!</h2>
+          <button style={{ cursor: "pointer" }} onClick={handleImageClick}>
+            Submit
+          </button>
+          <PopUpContact isOpen={isPopupOpen} closePopup={closePopup} />
+        </div>
+      </div>
     </div>
   );
 };
